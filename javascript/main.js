4029191ba0a4d706d61334986715ece8e5435b4e
snagavamsi123 <75851445+snagavamsi123@users.noreply.github.com>
2021-01-09 00:12:58 +0530

    Added from local storage
    Added all new files and old files toooo , @learn more page and watch video page with CSS

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,22 @@ if(this.hash != ''){
 }
 
 })
+
+
+//this is for desktop mode warning
+// $(document).ready(function(){    
+//     if(document.URL.indexOf("#")==-1){ //Check if the current URL contains '#'
+//         url = document.URL+"#"; // use "#". Add hash to URL
+//         location = "#";
+//         location.reload(true); //Reload the page
+//     }
+// });
+
+
+function desktopmode(){
+
+    if(window.location.href=='http://127.0.0.1:5500/index.html' || window.location.href=="http://127.0.0.1:5500"){
+        alert('Use Desktop Mode for better UI')
+    }
+
+}
